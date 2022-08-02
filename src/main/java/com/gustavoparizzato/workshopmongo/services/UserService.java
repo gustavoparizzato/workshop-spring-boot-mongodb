82acd99798aea37efe0de9e9a11afb892d77e0ae
Gustavo Parizzato <86897255+gustavoparizzato@users.noreply.github.com>
2022-08-02 17:42:13 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/gustavoparizzato/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.gustavoparizzato.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavoparizzato.workshopmongo.domain.User;
+import com.gustavoparizzato.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+}
