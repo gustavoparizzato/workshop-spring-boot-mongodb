a45d09a9676c5cabb8689ccd565fe222ed2ed58d
Gustavo Parizzato <86897255+gustavoparizzato@users.noreply.github.com>
2022-08-06 17:44:09 -0300

    User update with PUT

--- src/main/java/com/gustavoparizzato/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gustavoparizzato/workshopmongo/resources/UserResource.java
@@ -50,6 +50,14 @@ public class UserResource {
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+ 	public ResponseEntity<User> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = userService.fromDTO(objDto);
+		obj.setId(id);
+		obj = userService.update(obj);
+		return ResponseEntity.noContent().build();	
+	}
 }
 
 
